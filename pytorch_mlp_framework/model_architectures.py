6750170f5a41a11f7bd2cf30ab3685d93430c1a3
XZ-XuZhang <zhangxu852654@gmail.com>
2019-11-21 00:25:13 +0000

    added Resnet

--- pytorch_mlp_framework/model_architectures.py
+++ pytorch_mlp_framework/model_architectures.py
@@ -144,7 +144,6 @@ class EntryConvolutionalBlock(nn.Module):
 class ConvolutionalProcessingBlock(nn.Module):
     def __init__(self, input_shape, num_filters, kernel_size, padding, bias, dilation):
         super(ConvolutionalProcessingBlock, self).__init__()
-
         self.num_filters = num_filters
         self.kernel_size = kernel_size
         self.input_shape = input_shape
@@ -157,9 +156,10 @@ class ConvolutionalProcessingBlock(nn.Module):
         self.bn0 = nn.BatchNorm2d(self.num_filters)
         self.bn1 = nn.BatchNorm2d(self.num_filters)
         self.layer_dict = nn.ModuleDict()
+
         x = torch.zeros(self.input_shape)
         out = x
-
+        identity = out
         self.layer_dict['conv_0'] = nn.Conv2d(in_channels=out.shape[1], out_channels=self.num_filters, bias=self.bias,
                                               kernel_size=self.kernel_size, dilation=self.dilation,
                                               padding=self.padding, stride=1)
@@ -171,18 +171,21 @@ class ConvolutionalProcessingBlock(nn.Module):
                                               kernel_size=self.kernel_size, dilation=self.dilation,
                                               padding=self.padding, stride=1)
 
+
         out = self.layer_dict['conv_1'].forward(out)
+        out += identity
         out = F.leaky_relu(self.bn1(out))
 
         print(out.shape)
 
     def forward(self, x):
         out = x
-
+        identity=out
         out = self.layer_dict['conv_0'].forward(out)
         out = F.leaky_relu(self.bn0(out))
 
         out = self.layer_dict['conv_1'].forward(out)
+        out += identity
         out = F.leaky_relu(self.bn1(out))
 
         return out
@@ -208,6 +211,7 @@ class ConvolutionalDimensionalityReductionBlock(nn.Module):
         self.layer_dict = nn.ModuleDict()
         x = torch.zeros(self.input_shape)
         out = x
+        identity=out
 
         self.layer_dict['conv_0'] = nn.Conv2d(in_channels=out.shape[1], out_channels=self.num_filters, bias=self.bias,
                                               kernel_size=self.kernel_size, dilation=self.dilation,
@@ -222,20 +226,25 @@ class ConvolutionalDimensionalityReductionBlock(nn.Module):
                                               kernel_size=self.kernel_size, dilation=self.dilation,
                                               padding=self.padding, stride=1)
 
+        identity=nn.Conv2d(in_channels=out.shape[1], out_channels=self.num_filters,kernel_size=1,stride=2)(identity)
         out = self.layer_dict['conv_1'].forward(out)
+        out +=identity
         out = F.leaky_relu(self.bn1(out))
 
         print(out.shape)
 
     def forward(self, x):
         out = x
+        identity=out
 
         out = self.layer_dict['conv_0'].forward(out)
         out = F.leaky_relu(self.bn0(out))
 
         out = F.avg_pool2d(out, self.reduction_factor)
 
+        identity=nn.Conv2d(in_channels=out.shape[1], out_channels=self.num_filters,kernel_size=1,stride=2)(identity)
         out = self.layer_dict['conv_1'].forward(out)
+        out +=identity
         out = F.leaky_relu(self.bn1(out))
 
         return out
