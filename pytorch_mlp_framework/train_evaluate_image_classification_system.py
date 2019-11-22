3eeede0323d536b079398d38e69ed5cf5e1ae0f1
XZ-XuZhang <zhangxu852654@gmail.com>
2019-11-22 23:58:32 +0000

    added data augmentation

--- pytorch_mlp_framework/train_evaluate_image_classification_system.py
+++ pytorch_mlp_framework/train_evaluate_image_classification_system.py
@@ -16,9 +16,9 @@ torch.manual_seed(seed=args.seed)  # sets pytorch's seed
 transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(45)
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        transforms.RandomRotation(45)
     ])
 
 transform_test = transforms.Compose([
